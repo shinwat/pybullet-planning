b9b3fbe79a2d6a97e45afc143ff60954c29ffab5
Shin Watanabe <shinwa@ifi.uio.no>
2023-06-01 11:05:42 +0200

    add test for ikfast

--- examples/test_tiago_motion.py
+++ examples/test_tiago_motion.py
@@ -5,7 +5,9 @@ from __future__ import print_function
 import pybullet as p
 import time
 
-from pybullet_tools.tiago_utils import TIAGO_URDF, TUCKED_ARM, EXTENDED_ARM, TIAGO_GROUPS, open_gripper
+from pybullet_tools.tiago_problems import create_tiago
+from pybullet_tools.pr2_problems import create_floor, create_table
+from pybullet_tools.tiago_utils import TIAGO_TOOL_FRAME, TIAGO_URDF, TUCKED_ARM, EXTENDED_ARM, TIAGO_GROUPS, open_gripper, set_group_conf
 from pybullet_tools.utils import set_base_values, joint_from_name, quat_from_euler, set_joint_position, \
     set_joint_positions, add_data_path, connect, plan_base_motion, plan_joint_motion, enable_gravity, \
     joint_controller, dump_body, load_model, joints_from_names, wait_if_gui, disconnect, get_joint_positions, \
@@ -46,25 +48,44 @@ def test_arm_motion(tiago, arm_joints, arm_goal):
     print(len(arm_path))
     for q in arm_path:
         set_joint_positions(tiago, arm_joints, q)
-        #wait_if_gui('Continue?')
-        wait_for_duration(0.01)
+        if SLEEP is None:
+            wait_if_gui('Continue?')
+        else:
+            wait_for_duration(SLEEP)
 
+def test_ikfast(tiago):
+    wait_if_gui('Test IK?')
+    from pybullet_tools.ikfast.tiago.ik import get_tool_pose, get_ik_generator
+    left_joints = joints_from_names(tiago, TIAGO_GROUPS['arm'])
+    # torso_joints = joints_from_names(tiago, TIAGO_GROUPS['torso'])
+    # torso_left = torso_joints + left_joints
+    print(get_link_pose(tiago, link_from_name(tiago, TIAGO_TOOL_FRAME)))
+    # print(forward_kinematics('left', get_joint_positions(pr2, torso_left)))
+    print(get_tool_pose(tiago))
+
+    pose = get_tool_pose(tiago)
+    generator = get_ik_generator(tiago, pose, torso_limits=False)
+    for i in range(100):
+        solutions = next(generator)
+        print(i, len(solutions))
+        for q in solutions:
+            set_joint_positions(tiago, left_joints, q)
+            if SLEEP is None:
+                wait_if_gui('Continue?')
+            else:
+                wait_for_duration(SLEEP)
 #####################################
 
 def main():
     connect(use_gui=True)
     add_data_path()
 
-    plane = p.loadURDF("plane.urdf")
-    table_path = "models/table_collision/table.urdf"
-    table = load_pybullet(table_path, fixed_base=True)
+    plane = create_floor()
+    table = create_floor()
     set_quat(table, quat_from_euler(Euler(yaw=PI/2)))
-    obstacles = [table] # TODO: include plane in obstacles
+    obstacles = [table]
 
-    tiago_urdf = TIAGO_URDF
-    with HideOutput():
-        tiago = load_model(tiago_urdf, fixed_base=True) # TODO: suppress warnings?
-    dump_body(tiago)
+    tiago = create_tiago()
 
     base_start = (-2, -2, 0)
     base_goal = (2, 2, 0)
@@ -74,9 +95,12 @@ def main():
     arm_joints = joints_from_names(tiago, TIAGO_GROUPS['arm'])
     torso_joints = joints_from_names(tiago, TIAGO_GROUPS['torso'])
     set_joint_positions(tiago, arm_joints, arm_start)
-    set_joint_positions(tiago, torso_joints, [0.2])
+    set_group_conf(tiago, 'torso', [0.33])
     open_gripper(tiago)
     set_base_values(tiago, base_start)
+
+    # test inverse kinematics
+    test_ikfast(tiago)
 
     # test base motion
     add_line(base_start, base_goal, color=RED)
